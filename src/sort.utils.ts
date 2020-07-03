d1407d4e6363a08c2ea64bb69693dbd480368bf1
Maurits van Hassel <mcvanhassel@gmail.com>
2020-07-03 10:19:37 +0200

    Move sort implementation to its own function

--- src/sort.utils.ts
+++ src/sort.utils.ts
@@ -1,21 +1,30 @@
 type SortableType = string | boolean | number | bigint;
 
+enum SortDirection {
+  Ascending = 1,
+  Descending = -1,
+}
+
 export function sortAscending<T extends SortableType>(items: T[]): T[] {
   validateItems(items);
 
-  return [...items].sort((a, b) => compare(a, b));
+  return sort(items, SortDirection.Ascending);
 }
 
 export function sortDescending<T extends SortableType>(items: T[]): T[] {
   validateItems(items);
 
-  return [...items].sort((a, b) => -1 * compare(a, b));
+  return sort(items, SortDirection.Descending);
 }
 
 function validateItems<T>(items: T[]): void {
   if (items === null || items === undefined || !Array.isArray(items)) {
     throw new Error('Argument "items" should be provided as an array');
   }
+}
+
+function sort<T extends SortableType>(items: T[], direction: SortDirection): T[] {
+  return [...items].sort((a, b) => direction * compare(a, b));
 }
 
 function compare(a: SortableType, b: SortableType): number {
