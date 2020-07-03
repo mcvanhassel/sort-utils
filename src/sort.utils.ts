f560579094e5393e640edcc86ae4694588526a65
Maurits van Hassel <mcvanhassel@gmail.com>
2020-07-03 10:00:13 +0200

    Move sort compare to its own function

--- src/sort.utils.ts
+++ src/sort.utils.ts
@@ -1,9 +1,15 @@
-export function sortAscending<T>(items: T[]): T[] {
+type SortableType = string | boolean | number | bigint;
+
+export function sortAscending<T extends SortableType>(items: T[]): T[] {
   if (items === null || items === undefined || !Array.isArray(items)) {
     throw new Error('Argument "items" should be provided as an array');
   }
 
-  return [...items].sort((a, b) => (typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b) : a > b ? 1 : a < b ? -1 : 0));
+  return [...items].sort((a, b) => compare(a, b));
 }
 
 export function sortDescending() {}
+
+function compare(a: SortableType, b: SortableType): number {
+  return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b) : a > b ? 1 : a < b ? -1 : 0;
+}
