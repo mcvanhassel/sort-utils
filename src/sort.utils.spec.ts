2a9eccf3310b8532f669aa70db4f8e64385cad3f
Maurits van Hassel <mcvanhassel@gmail.com>
2020-07-03 10:07:05 +0200

    Implement sort descending function

--- src/sort.utils.spec.ts
+++ src/sort.utils.spec.ts
@@ -71,5 +71,65 @@ describe('sort utils', () => {
     it('is defined', () => {
       expect(sortDescending).toBeDefined();
     });
+
+    it('should throw an error when argument "items" is not provided', () => {
+      expect(sortDescending).toThrow();
+    });
+
+    it('should throw an error when argument "items" is not an array', () => {
+      expect(() => sortDescending(123 as any)).toThrow('Argument "items" should be provided as an array');
+    });
+
+    it('should not throw an error when argument "items" is an array', () => {
+      expect(() => sortDescending([1, 2, 3])).not.toThrow();
+    });
+
+    it('should return an array', () => {
+      const result = sortDescending([]);
+
+      expect(result).toBeInstanceOf(Array);
+    });
+
+    it('should sort string items', () => {
+      const items = ['c', 'a', 'b'];
+      const result = sortDescending(items);
+
+      expect(result).toEqual(['c', 'b', 'a']);
+    });
+
+    it('should sort number items', () => {
+      const items = [3, 10, 2, 1];
+      const result = sortDescending(items);
+
+      expect(result).toEqual([10, 3, 2, 1]);
+    });
+
+    it('should sort bigint items', () => {
+      const items = [BigInt('90071992547409990'), BigInt('1007199254740999'), BigInt('9007199254740999')];
+      const result = sortDescending(items);
+
+      expect(result).toEqual([BigInt('90071992547409990'), BigInt('9007199254740999'), BigInt('1007199254740999')]);
+    });
+
+    it('should sort boolean items', () => {
+      const items = [true, false, false, true, false];
+      const result = sortDescending(items);
+
+      expect(result).toEqual([true, true, false, false, false]);
+    });
+
+    it('should sort string items with special characters', () => {
+      const items = ['ä', 'é', 'e', 'è', 'b', 'ü', 'a', 'u'];
+      const result = sortDescending(items);
+
+      expect(result).toEqual(['ü', 'u', 'è', 'é', 'e', 'b', 'ä', 'a']);
+    });
+
+    it('should not mutate input argument "items"', () => {
+      const items = ['c', 'a', 'b'];
+      sortDescending(items);
+
+      expect(items).toEqual(['c', 'a', 'b']);
+    });
   });
 });
--- src/sort.utils.ts
+++ src/sort.utils.ts
@@ -8,7 +8,13 @@ export function sortAscending<T extends SortableType>(items: T[]): T[] {
   return [...items].sort((a, b) => compare(a, b));
 }
 
-export function sortDescending() {}
+export function sortDescending<T extends SortableType>(items: T[]): T[] {
+  if (items === null || items === undefined || !Array.isArray(items)) {
+    throw new Error('Argument "items" should be provided as an array');
+  }
+
+  return [...items].sort((a, b) => -1 * compare(a, b));
+}
 
 function compare(a: SortableType, b: SortableType): number {
   return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b) : a > b ? 1 : a < b ? -1 : 0;
